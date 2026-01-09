6aa75a49b6853b40d9fe874dd67a040d81fe7907
isaacvigil <isaacvigil@gmail.com>
2026-01-09 13:17:04 +0100

    Avatar vertical alignment, home copy

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
             <h2 className="text-2xl md:text-3xl lg:text-4xl text-muted-foreground">
             TrackLight extracts details from job post links,
             </h2>
-            <h2 className="text-2xl md:text-3xl lg:text-4xl text-muted-foreground">so you can focus on applying</h2>
+            <h2 className="text-2xl md:text-3xl lg:text-4xl text-muted-foreground">so you can save time and focus on getting hired</h2>
           </div>
           <HomepageSignUpCta />
         </div>
