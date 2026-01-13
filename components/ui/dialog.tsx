0747a1a886100ad0cd196b63627d063aa5390092
isaacvigil <isaacvigil@gmail.com>
2026-01-13 19:05:47 +0100

    Dialog title improvements

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -110,7 +110,7 @@ function DialogTitle({
   return (
     <DialogPrimitive.Title
       data-slot="dialog-title"
-      className={cn("text-lg leading-none font-medium", className)}
+      className={cn("text-lg leading-snug font-medium pr-10", className)}
       {...props}
     />
   )
